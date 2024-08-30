f29a6aa9b7646a959a5ebd0de4e6a0ed83e99bdb
JorgeRangel <jorgelrgarduno@gmail.com>
2024-08-30 10:20:51 -0600

    Refactor: Convert functions to arrow functions in Card components

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,9 +1,11 @@
 import BackCard from "./components/BackCard"
 import FrontCard from "./components/FrontCard"
 
-export default function Cards() {
-  return <section className="lg:grid lg:grid-cols-6 lg:gap-6 lg:grid-rows-2 static">
+const Cards = () => (
+  <section className="lg:grid lg:grid-cols-6 lg:gap-6 lg:grid-rows-2 static">
     <FrontCard />
     <BackCard />
   </section>
-}
+)
+
+export default Cards
--- src/components/Cards/components/FrontCard.tsx
+++ src/components/Cards/components/FrontCard.tsx
@@ -1,25 +1,24 @@
-export default function FrontCard() {
-    return (
-        <div className="bg-[url('assets/images/bg-card-front.png')] bg-cover w-72 h-40 md:w-96 md:h-52 rounded-lg p-6 lg:col-start-1 lg:col-end-4 lg:static absolute top-28 left-5 sm:top-32 sm:left-6 z-10">
-            <div className="text-white text-light-grayish-violet flex flex-col h-full justify-between">
+const FrontCard = () => (
+    <div className="bg-[url('assets/images/bg-card-front.png')] bg-cover w-72 h-40 md:w-96 md:h-52 rounded-lg p-6 lg:col-start-1 lg:col-end-4 lg:static absolute top-28 left-5 sm:top-32 sm:left-6 z-10">
+        <div className="text-white text-light-grayish-violet flex flex-col h-full justify-between">
 
-                <div className="flex flex-row items-center">
-                    <div className="w-7 h-7 md:w-10 md:h-10 rounded-full border-2 border-white bg-white" />
-                    <div className="w-3 h-3 md:w-5 md:h-5 rounded-full border border-white ml-2 md:ml-4" />
+            <div className="flex flex-row items-center">
+                <div className="w-7 h-7 md:w-10 md:h-10 rounded-full border-2 border-white bg-white" />
+                <div className="w-3 h-3 md:w-5 md:h-5 rounded-full border border-white ml-2 md:ml-4" />
+            </div>
+
+            <div className="flex flex-col h-1/2 justify-around">
+                <p className="w-full text-lg md:text-2xl text-left tracking-widest">
+                    0000 0000 0000 0000
+                </p>
+                <div className="flex justify-between text-xs md:text-sm uppercase tracking-widest">
+                    <p>Jane Appleseed</p>
+                    <p>00/00</p>
                 </div>
-
-                <div className="flex flex-col h-1/2 justify-around">
-                    <p className="w-full text-lg md:text-2xl text-left tracking-widest">
-                        0000 0000 0000 0000
-                    </p>
-                    <div className="flex justify-between text-xs md:text-sm uppercase tracking-widest">
-                        <p>Jane Appleseed</p>
-                        <p>00/00</p>
-                    </div>
-                </div>
-
             </div>
 
         </div>
-    )
-}
+    </div>
+)
+
+export default FrontCard
