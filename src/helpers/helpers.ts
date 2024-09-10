08f1c835c59b7a50f410e53c85f364921e1b1d8b
JorgeRangel <jorgelrgarduno@gmail.com>
2024-09-10 11:36:36 -0600

    Format helpers file and remove semicolons

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -9,10 +9,10 @@ export const formatCardNumber = (cardNumber: string) => {
 export const formatMonth = (month: string): string => {
     const numMonth = Number(month)
     if (isNaN(numMonth) || numMonth < 1 || numMonth > 12) {
-        return '';
+        return ''
     }
-    return numMonth < 10 ? `0${numMonth}` : `${numMonth}`;
-};
+    return numMonth < 10 ? `0${numMonth}` : `${numMonth}`
+}
 
 export const formatYear = (year: string): string => {
     const numYear = Number(year)
@@ -20,34 +20,9 @@ export const formatYear = (year: string): string => {
         return ''
     }
     return numYear < 10 ? `0${numYear}` : `${numYear}`
-};
+}
 
 export const formatCVC = (cvc: string): string => {
-    const cleanedCVC = cvc.replace(/\D+/g, '');
+    const cleanedCVC = cvc.replace(/\D+/g, '')
     return cleanedCVC.slice(0, 3);
-};
-
-
-
-
-export const validateCardNumber = (cardNumber: string) => {
-    // Verificar si no está en blanco
-    if (cardNumber.trim() === '') {
-        return { isValid: false, errorMessage: "Can't be blank" };
-    }
-
-    // Verificar si tiene solo números
-    const cleaned = cardNumber.replace(/\D+/g, '');
-    if (cleaned.length !== cardNumber.length) {
-        return { isValid: false, errorMessage: "Wrong format, numbers only" };
-    }
-
-    // Verificar si tiene 16 dígitos
-    if (cleaned.length !== 16) {
-        return { isValid: false, errorMessage: "Card number must be 16 digits long" };
-    }
-
-    // Si pasa las validaciones
-    return { isValid: true, errorMessage: "" };
-};
-
+}
